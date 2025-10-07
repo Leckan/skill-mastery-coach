9233f359c9cdd1b703d298c87aa4658c7e6b57af
Leckan <ahmed2real@gmail.com>
2025-10-07 02:25:27 -0700

    ai resume agent inngest function

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,13 +1,14 @@
 import { serve } from "inngest/next";
-import { AiCareerAgent, helloWorld } from "@/inngest/functions";
-import { inngest } from "@/inngest/client";
+import { inngest } from "../../../inngest/client";
+import { AiCareerAgent, AiResumeAgent, AIRoadmapAgent, helloWorld } from "@/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    helloWorld,
-    AiCareerAgent,
-  ],
+    client: inngest,
+    functions: [
+        /* your functions will be passed here later! */
+        AiCareerAgent,
+        AiResumeAgent,
+        AIRoadmapAgent
+    ],
 });
