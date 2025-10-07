c4f3209765c1e5b3e05baf147e9e1c0208a6a832
Leckan <ahmed2real@gmail.com>
2025-10-06 21:57:10 -0700

    inngest

--- app/(routes)/dashboard/_components/AiToolCard.tsx
+++ app/(routes)/dashboard/_components/AiToolCard.tsx
@@ -1,32 +1,36 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 export interface TOOL {
-    name: string,
-    desc: string,
-    icon: string,
-    button: string,
-    path: string
+  name: string;
+  desc: string;
+  icon: string;
+  button: string;
+  path: string;
 }
 
 type AIToolProps = {
-    tool: TOOL
-}
+  tool: TOOL;
+};
 
-function AiToolCard({tool}: AIToolProps) {
+function AiToolCard({ tool }: AIToolProps) {
   return (
-    <div className='p-3 border rounded-lg '>
-            <Image src={tool.icon} width={40} height={40} alt={tool.name} />
-            <h2 className='font-bold mt-2'>{tool.name}</h2>
-            <p className='text-gray-400'>{tool.desc}</p>
+    <div className="p-3 border rounded-lg ">
+      <Image src={tool.icon} width={40} height={40} alt={tool.name} />
+      <h2 className="font-bold mt-2">{tool.name}</h2>
+      <p className="text-gray-400">{tool.desc}</p>
 
-            <Button className='w-full mt-3'
-                //onClick={onClickButton}
-            >{tool.button}</Button>
-
-           
-        </div>
-  )
+      <Link href={tool.path}>
+        <Button
+          className="w-full mt-3"
+          //onClick={onClickButton}
+        >
+          {tool.button}
+        </Button>
+      </Link>
+    </div>
+  );
 }
 
-export default AiToolCard
+export default AiToolCard;
